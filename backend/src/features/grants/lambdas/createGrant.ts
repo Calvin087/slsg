917291484161238eddfc217040a8613cc02be9ef
Calvin T <58273749+Calvin087@users.noreply.github.com>
2025-10-11 14:44:44 +0200

    set up lambdas and types

---
+++ backend/src/features/grants/lambdas/createGrant.ts
@@ -0,0 +1,12 @@
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+
+const createGrant = async (
+  event: APIGatewayProxyEventV2,
+): Promise<APIGatewayProxyResultV2> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "ok" }),
+  };
+};
+
+export const handler = createGrant;
