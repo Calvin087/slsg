04bc560cdfcc25f4c3343c72c1b6e15c157ef571
Calvin T <58273749+Calvin087@users.noreply.github.com>
2025-10-12 06:49:14 +0200

    created delete grant lambda

--- backend/src/features/grants/lambdas/deleteGrant.ts
+++ backend/src/features/grants/lambdas/deleteGrant.ts
@@ -1,12 +1,29 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { DynamoAdapter } from "../../../db/dynamo";
 
 const deleteGrant = async (
   event: APIGatewayProxyEventV2,
 ): Promise<APIGatewayProxyResultV2> => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "ok" }),
-  };
+  // @ts-ignore
+  const userSubId = event.requestContext?.authorizer?.claims?.sub;
+  if (!userSubId) throw new Error("Missing user sub id");
+
+  const grantId = event.pathParameters?.id;
+  if (!grantId) throw new Error("Missing path param");
+
+  try {
+    await DynamoAdapter.delete(userSubId, grantId);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Grant deleted" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Failed to delete grant", error }),
+    };
+  }
 };
 
 export const handler = deleteGrant;
