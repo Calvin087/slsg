76fcf2e4df843a90a06bda5cf3212e5f4f7b1194
Calvin T <58273749+Calvin087@users.noreply.github.com>
2025-10-12 06:43:55 +0200

    created get grant lambda

--- backend/src/features/grants/lambdas/getGrant.ts
+++ backend/src/features/grants/lambdas/getGrant.ts
@@ -1,12 +1,38 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { DynamoAdapter } from "../../../db/dynamo";
 
 const getGrant = async (
   event: APIGatewayProxyEventV2,
 ): Promise<APIGatewayProxyResultV2> => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "ok" }),
-  };
+  // @ts-ignore
+  const userSubId = event.requestContext?.authorizer?.claims?.sub;
+  if (!userSubId) throw new Error("Missing user sub id");
+
+  const grantId = event.pathParameters?.id;
+  if (!grantId) throw new Error("Missing path param");
+
+  try {
+    const data = await DynamoAdapter.getItem(userSubId, grantId);
+
+    if (!data) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "Grant not found" }),
+      };
+    }
+
+    const { userSubId: _, ...rest } = data;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data: rest }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to fetch grant", error }),
+    };
+  }
 };
 
 export const handler = getGrant;
